16f4b1848602c33cf101224535d85eabe40abda3
YILIN <YILIN@DESKTOP-YILIN>
2022-08-11 23:42:12 +0800

    fx

--- DSCpp/DSCpp.cpp
+++ DSCpp/DSCpp.cpp
@@ -140,14 +140,35 @@ void QuickSort(int data[], int n)
 {
 	QuickSortImpl(data, 0, n - 1);
 }
-__m256i bitmap2vecmask(__mmask8 m) {
+__m256i expand_bits_to_bytes(uint32_t x)
+{
+	__m256i xbcast = _mm256_set1_epi32(x);    // we only use the low 32bits of each lane, but this is fine with AVX2
+
+	// Each byte gets the source byte containing the corresponding bit
+	__m256i shufmask = _mm256_set_epi64x(
+		0x0303030303030303, 0x0202020202020202,
+		0x0101010101010101, 0x0000000000000000);
+	__m256i shuf = _mm256_shuffle_epi8(xbcast, shufmask);
+
+	__m256i andmask = _mm256_set1_epi64x(0x8040201008040201);  // every 8 bits -> 8 bytes, pattern repeats.
+	__m256i isolated_inverted = _mm256_andnot_si256(shuf, andmask);
+
+	// this is the extra step: compare each byte == 0 to produce 0 or -1
+	return _mm256_cmpeq_epi8(isolated_inverted, andmask/*_mm256_setzero_si256()*/);
+	// alternative: compare against the AND mask to get 0 or -1,
+	// avoiding the need for a vector zero constant.
+}
+
+__m256i _mm256i_convert_mask8_to_epi32(__mmask8 m) {
 	const __m256i vshift_count = _mm256_set_epi32(24, 25, 26, 27, 28, 29, 30, 31);
 	__m256i bcast = _mm256_set1_epi32(m);
 	__m256i shifted = _mm256_sllv_epi32(bcast, vshift_count);  // high bit of each element = corresponding bit of the mask
 
 	// use _mm256_and and _mm256_cmpeq if you need all bits set.
-	return _mm256_srai_epi32(shifted, 31);             // broadcast the sign bit to the whole element
+	return _mm256_srai_epi32(shifted, 31); 
+	// broadcast the sign bit to the whole element
 }
+
 void __cdecl _mm256_mask_i32scatter_epi32_avx2(void* base_addr, __mmask8 k, __m256i vindex, __m256i a, const int scale)
 {
 	//FOR j : = 0 to 7
@@ -201,7 +222,7 @@ void __cdecl _mm256_mask_i32scatter_epi32_avx2(void* base_addr, __mmask8 k, __m2
 #define _mm256_cmpeq_epi16_mask(a,b) (__mmask8)_mm256_movemask_ps(_mm256_castsi256_ps(_mm256_cmpeq_epi16(b, a)))
 #endif
 #ifndef _mm256_mask_blend_epi32
-#define _mm256_mask_blend_epi32(mask, a,b) _mm256_castps_si256(_mm256_blendv_ps(_mm256_castsi256_ps(a),_mm256_castsi256_ps(b), _mm256_castsi256_ps(bitmap2vecmask(mask))))
+#define _mm256_mask_blend_epi32(mask, a,b) _mm256_castps_si256(_mm256_blendv_ps(_mm256_castsi256_ps(a),_mm256_castsi256_ps(b), _mm256_castsi256_ps(_mm256i_convert_mask8_to_epi32(mask))))
 #endif
 
 #ifndef _mm256_mask_i32scatter_epi32
